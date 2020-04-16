d8b7b801add6654342acda8cf1b875c2d3564633
Mojo <1316578052@qq.com>
2020-04-17 06:48:19 +0800

    3

--- src/main/java/cn/linyt/againhospitalnevermore/controller/AdminUserController.java
+++ src/main/java/cn/linyt/againhospitalnevermore/controller/AdminUserController.java
@@ -7,6 +7,7 @@ import cn.linyt.againhospitalnevermore.utils.JwtTokenUtil;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ import java.util.UUID;
  * @Version 1.0
  **/
 @Slf4j
+@CrossOrigin
 @RestController
 public class AdminUserController {
 
