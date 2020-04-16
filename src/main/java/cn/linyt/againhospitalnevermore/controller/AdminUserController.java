daa8af5b91701601f06e85b75714fa1db55a71ba
Mojo <1316578052@qq.com>
2020-04-17 06:27:50 +0800

    2

---
+++ src/main/java/cn/linyt/againhospitalnevermore/controller/AdminUserController.java
@@ -0,0 +1,55 @@
+package cn.linyt.againhospitalnevermore.controller;
+
+import cn.linyt.againhospitalnevermore.annotation.JwtIgnore;
+import cn.linyt.againhospitalnevermore.entity.Audience;
+import cn.linyt.againhospitalnevermore.response.Result;
+import cn.linyt.againhospitalnevermore.utils.JwtTokenUtil;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+/**
+ * @ClassName AdminUserController
+ * @Description TODO
+ * @Author Mojo
+ * @Date 2020/4/14 5:02
+ * @Version 1.0
+ **/
+@Slf4j
+@RestController
+public class AdminUserController {
+
+    @Autowired
+    private Audience audience;
+
+    @PostMapping("/login")
+    @JwtIgnore
+    public Result adminLogin(HttpServletResponse response, String username, String password) {
+
+        // 这里模拟测试, 默认登录成功，返回用户ID和角色信息
+        if (!"admin".equals(username) && !"666666".equals(password)) {
+            log.info("### 用户名或密码错误! ###");
+            return Result.FAIL("用户名或密码错误!");
+        }
+        String userId = UUID.randomUUID().toString();
+        String role = "admin";
+
+        // 创建token
+        String token = JwtTokenUtil.createJWT(userId, username, role, audience);
+        log.info("### 登录成功, token={} ###", token);
+
+        // 将token放在响应头
+        response.setHeader(JwtTokenUtil.AUTH_HEADER_KEY, JwtTokenUtil.TOKEN_PREFIX + token);
+        // 将token响应给客户端
+        JSONObject result = new JSONObject();
+        result.put("token", token);
+        return Result.SUCCESS(result);
+    }
+
+}
