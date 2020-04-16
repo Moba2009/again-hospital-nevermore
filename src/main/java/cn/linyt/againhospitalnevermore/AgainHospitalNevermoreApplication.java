954f24d36c770aa0dd63ae911305c7877d7514c6
Mojo <1316578052@qq.com>
2020-04-17 05:49:53 +0800

    0

---
+++ src/main/java/cn/linyt/againhospitalnevermore/AgainHospitalNevermoreApplication.java
@@ -0,0 +1,13 @@
+package cn.linyt.againhospitalnevermore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgainHospitalNevermoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgainHospitalNevermoreApplication.class, args);
+    }
+
+}
